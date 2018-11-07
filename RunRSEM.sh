110583911c8971f505562beea152c0941768a4c6
Ross Blanchard <blanchard.ross@gmail.com>
2018-11-06 21:36:55 -0800

    abstractify RunRSEM.sh file

--- RunRSEM.sh
+++ RunRSEM.sh
@@ -1,38 +1,61 @@
-#!bin/bash
+#!/bin/bash
 
-stardir='RNASEQ_data/star_REP'
-rsemdir='RNASEQ_data/rsem_REP'
-mkdir $stardir
-mkdir $rsemdir
+readonly usage="
+Usage: RunRSEM.sh -p project_name
 
-# get the list of FASTQ files.
-ARRAY=(`find /data/RAW_DATA/ -name "*fastq.gz"  -printf %f!`)
-ParentPATH="/data/RAW_DATA/Project_16494_FUID1031578/"
-touch ProcessedRSEM.txt
+    -p         project name: name of the project you're working on.
+"
 
-ARRAY=(`ls $ParentPATH`)
+# parsing provided arguments
+while getopts p: opt; do
+    case "$opt" in
+        p)   project_name="$OPTARG";;
+        '?') echo "$usage" >&2; exit 1;;
+        ':') echo "option $OPTARG requires an argument" >&2; exit 1;;
+        '') echo 
+    esac
+done
+shift "$(($OPTIND-1))"
 
-#For each path, run STAR and RSEM
-for i in ${ARRAY[*]}; do
+if [[ -z $project_name ]]; then
+  echo "project_name argument must be provided."
+  echo "$usage"
+  exit 1
+else
+  project_path="./data/RAW_DATA/${project_name}"
+fi
 
-    echo $ParentPATH$i
-    ARRAY2=(`find $ParentPATH$i -name "*fastq.gz"`)
-    NAME=`basename ${ARRAY2[0]} .fastq.gz`
-   echo "making directory: RNASEQ_data/star_REP"
+star_dir="./data/RNASEQ_data/star_REP"
+rsem_dir="./data/RNASEQ_data/rsem_REP"
+mkdir -p "$star_dir" "$rsem_dir"
+
+# get the list of FASTQ filepaths.
+fastq_filepaths=(`find "$project_path" -name "*.fastq.gz"`)
+
+# for each FASTQ file, run STAR and RSEM
+for filename in "${fastq_filepaths[@]}"; do
+  # using regex to parse the name of the analysis from the filename
+  analysis_name="$(expr "$filename" : '.*/\(.*\).fastq.gz')"
+  echo "Processing analysis $analysis_name"
 
   # RUN THE STAR ALIGNMENT
-   /home/jabust/STAR-2.6.0a/bin/Linux_x86_64/STAR --genomeDir /home/jabust/genome --readFilesCommand zcat \
-  --readFilesIn ${ARRAY2[*]} --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 16000000000 --outSAMunmapped Within \
+  # TODO: pass genome dir as an argument to script?
+  /home/jabust/STAR-2.6.0a/bin/Linux_x86_64/STAR --genomeDir /home/jabust/genome --readFilesCommand zcat \
+  --readFilesIn "${filename}" --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 16000000000 --outSAMunmapped Within \
   --twopassMode Basic --outFilterMultimapNmax 1 --quantMode TranscriptomeSAM \
-  --runThreadN 20 --outFileNamePrefix "RNASEQ_data/star_REP/"$NAME;
+  --runThreadN 20 --outFileNamePrefix "${star_dir}/${analysis_name}";
 
-   # RUN THE RSEM ALIGNMENT	
-   software/RSEM-1.2.25/rsem-calculate-expression --bam --no-bam-output -p 20 \
---paired-end --forward-prob 0 \
-"RNASEQ_data/star_REP/"$NAME"Aligned.toTranscriptome.out.bam" \
-ref/HomoSapStar_ref RNASEQ_data/rsem_REP/$NAME'_rsem' >& \
-RNASEQ_data/rsem_REP/$NAME'_rsem.log'
+  # RUN THE RSEM ALIGNMENT
+  # TODO: pass HomoSapStar_ref dir as an argument to script?
+  # TODO: is that Aligned filename right? Should there be a '.' before 'Aligned'?
+  software/RSEM-1.2.25/rsem-calculate-expression --bam --no-bam-output -p 20 \
+  --paired-end --forward-prob 0 \
+  "RNASEQ_data/star_REP/${analysis_name}Aligned.toTranscriptome.out.bam" \
+  "ref/HomoSapStar_ref" "${rsem_dir}/${analysis_name}_rsem" >& \
+  "${rsem_dir}/${analysis_name}_rsem.log"
 
-echo $NAME >> ProcessedRSEM.txt
+  # writing the analyzed filename to another file
+  # TODO: figure out why this is done
+  echo "${analysis_name}" >> ProcessedRSEM.txt
 
 done
